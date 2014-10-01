4c5f8181c195497446c3e08080dad4aeb7ec2a24
GongPA <david.chen@spsmail.cuny.edu>
2014-10-01 18:04:15 -0400

    Update task_01.py
    got idea from Quiz 6

--- task_01.py
+++ task_01.py
@@ -1,1 +1,32 @@
-#
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" 
+    task_01.py
+    Even and Odds() 
+"""
+
+
+def evens_and_odds(numbers, show_even=True):
+  """
+    accepts numbers as list object
+    and show_even as bool...
+    returns new list of even or odd numbers
+  """
+
+  listout = []
+
+    if show_even:
+        for i in numbers:
+            if i % 2 == 0:
+                listout.append(i)
+
+    elif not show_even:
+        for i in numbers:
+            if i % 2 != 0:
+                listout.append(i)
+    else:
+        print None
+        return None
+
+    return listout
