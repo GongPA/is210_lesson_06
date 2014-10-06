66e2e8a299513cd921990680f981b4eb25295e2f
GongPA <david.chen@spsmail.cuny.edu>
2014-10-06 00:08:17 -0400

    add list one task

--- task_03.py
+++ task_03.py
@@ -30,13 +30,13 @@ def bubble_sort(input_list):
     '''bubble sort function'''
 
     done = False
-while not done:
+    while not done:
     done = True
-    for i in range(len(input_list) - 1):
-        if input_list[i] > input_list[i + 1]:
-            done = False
-        input_list[i], input_list[i + 1] = input_list[i + 1], input_list[i]
-    """
-    here also can using a temp for swipe list position
-    """
-return list
+        for i in range(len(input_list) - 1):
+            if input_list[i] > input_list[i + 1]:
+                done = False
+            input_list[i], input_list[i + 1] = input_list[i + 1], input_list[i]
+        """
+        here also can using a temp for swipe list position
+        """
+    return list
