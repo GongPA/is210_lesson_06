d53dcdd260266614020f484e18a4f647bada29ca
GongPA <david.chen@spsmail.cuny.edu>
2014-10-06 00:22:29 -0400

    add list one task

--- task_03.py
+++ task_03.py
@@ -2,27 +2,8 @@
 # -*- coding: utf-8 -*-
 
 """
-  task_03.py
-  Bubble Sort
-my_list = [13,5,86,17,8,9,10]
-done = False
-while not done:
-  done = True
-  for i in range(len(my_list) - 1):
-    if my_list[i] > my_list[i + 1]:
-      '
-        compare the elements in list form index 0 - the end of list
-        example: [13,5,86,17,8,9,10]
-        5<86
-        but 13>5
-        therefor it sets the trigger 'done' to active.
-        which is false to kick in the while loop.
-      '
-      done = False
-      my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
-
-
-print my_list
+    task_03.py
+    Bubble Sort
 """
 
 import data
